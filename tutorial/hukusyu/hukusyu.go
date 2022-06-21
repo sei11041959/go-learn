aa142c26ce3d3dc9e1e1cb96a89a94189273c5bf
sei11041959 <sei1104.idea@gmail.com>
2022-06-21 16:30:22 +0900

    復習

--- tutorial/hukusyu/hukusyu.go
+++ tutorial/hukusyu/hukusyu.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("復習！")
+	fmt.Println("addの戻り値は", add(7, 9), "です")
+	fmt.Println("multipledの戻り値は", multipled(7, 9), "です")
+}
+func add(x, y int) (r int) {
+	fmt.Println("こんにちは!func add()です!")
+	r = x + y
+	fmt.Println("引数の", x, "と", y, "を足して", r, "になったよ!", "戻り値としてこれを返すね!")
+	return
+}
+func multipled(x, y int) (r int) {
+	fmt.Println("こんにちは!func multipledです!")
+	r = x * y
+	fmt.Println("引数の", x, "と", y, "を足して", r, "になったよ!", "戻り値としてこれを返すね!")
+	return
 }
